2f0e2dbfe0cd06c4df114b41cc8d98b65fa4abed
Patrik Tomek <pataz23@seznam.cz>
2022-11-07 13:51:22 +0100

    Delete item btn

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Item = (props) => {
+
   const handleIsChecked = (e) => {
     e.currentTarget.classList.toggle("checked-item");
+  };
+  const handleDelete = (e) => {
+    e.currentTarget.parentElement.remove();
   };
 
   return (
     <div className="item" onClick={handleIsChecked}>
-      {props.text}
+      <div>{props.text}</div>
+        <button className="delete-item_btn" onClick={handleDelete}>
+          D
+        </button>
     </div>
   );
 };
